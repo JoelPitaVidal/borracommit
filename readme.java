eaa15bde05616325dd57cb99e1bab0de17a36a8f
Joel Pita Vidal <jpitavidal@danielcastelao.org>
2024-01-12 09:08:18 +0100

    commit 1

---
+++ readme.java
@@ -0,0 +1,9 @@
+public class readme {
+}
+//Hacer algo con un commit anterior
+
+//Amend añadir ficheros al commit ya echo
+
+//git restore devuelve el archivo al estado en el que estaba al realizar el ultimo commit
+
+//git restore stage
